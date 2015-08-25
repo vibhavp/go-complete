47135e4c8a2792f5c3f50e1cbdd4f7051077fe42
Vibhav Pant <vibhavp@gmail.com>
2015-08-25 14:33:59 +0530

    Add usage instructtions.

--- go-complete.el
+++ go-complete.el
@@ -10,7 +10,9 @@
 ;;; Commentary:
 ;; This package provides native code completion for the Go Programming
 ;; Language.
-;; To enable, put:
+;; To enable, ad the following code into your .emacs:
+;; (require 'go-complete)
+;; (add-hook 'completion-at-point-functions 'go-complete-at-point)
 
 ;;; License:
 
