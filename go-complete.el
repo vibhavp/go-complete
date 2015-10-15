e39efc356f6e19f17db3f3d2c81f28d38b31a55e
Vibhav Pant <vibhavp@gmail.com>
2015-10-15 14:58:38 +0530

    Add gocode info.

--- go-complete.el
+++ go-complete.el
@@ -13,6 +13,8 @@
 ;; To enable, ad the following code into your .emacs:
 ;; (require 'go-complete)
 ;; (add-hook 'completion-at-point-functions 'go-complete-at-point)
+;; You need to have gocode (https://github.com/nsf/gocode) installed to use
+;; this package.
 
 ;;; License:
 
